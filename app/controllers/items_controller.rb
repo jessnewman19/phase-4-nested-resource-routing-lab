e55992b1a93d977e133829a7290db57e29fbd8c3
Jessica <jess.newman19@gmail.com>
2022-06-08 13:53:55 -0400

    commit tree e5a0de41fc7547a71577175bc872a0bdc2f07500

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,28 @@
 class ItemsController < ApplicationController
 
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
     items = Item.all
+    end
     render json: items, include: :user
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: 404
+  end
+
+  def show 
+    item = Item.find(params[:id])
+    render json: item 
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Item not found" }, status: 404
+  end
+
+  def create 
+    user = User.find(params[:user_id])
+    item = user.items.create!(params.permit(:name, :description, :price))
+    render json: item, status: :created
   end
 
 end
